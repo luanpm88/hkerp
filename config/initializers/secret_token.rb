32da7df6bd7e5a19bc711facfdf5e02da5771310
Luan Pham <luanm88@gmail.com>
2013-11-09 05:14:16 +0400

    init first hkerp project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hkerp::Application.config.secret_key_base = '1ad79552f9578c3120cb23f00d4d264857dc70a2c776c2fcc43322f8d7ad20c16af9ca98e9344703c6275d75865a273e239fa6da38dc6a77bd9ec96963e4961c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hkerp::Application.config.session_store :cookie_store, key: '_hkerp_session'
