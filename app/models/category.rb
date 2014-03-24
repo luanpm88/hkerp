3d760e638dcadb19ca20cc9fc5b36dbd9b18dc5c
Luan Pham <luanpm88@gmail.com>
2014-03-24 16:41:21 +0700

    fix some performance

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,4 +7,9 @@ class Category < ActiveRecord::Base
   has_many :parent, :through => :parent_categories, :source => :parent
   has_many :child_categories, :class_name => "ParentCategory", :foreign_key => "parent_id", :dependent => :destroy
   has_many :children, :through => :child_categories, :source => :category
+  
+  def update_level(lvl)
+    self.level = lvl
+    self.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Hkerp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'products#index'
+  root 'orders#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
