21a45e46067a0159f2e194aa48c6fd9a7de06cf7
Luan Pham <luanpm88@gmail.com>
2015-03-28 01:05:31 -0700

    test email hosting config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Hkerp::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => '27.0.15.181:3000' }
   
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
