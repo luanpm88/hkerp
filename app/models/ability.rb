bbe9446995129c2e3ef6e0efc90b9b376dd873cb
Luan Pham <luanpm88@gmail.com>
2015-04-02 18:18:23 -0700

    right for salesperson change item with price confirmed status

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -117,10 +117,10 @@ class Ability
                             
         end        
         can :change, Order do |order|
-          order.is_sales && order.salesperson_id == user.id && order.status.name == 'confirmed'
+          order.is_sales && order.salesperson_id == user.id && ['price_confirmed','confirmed'].include?(order.status.name)
         end
         can :do_change, Order do |order|
-          order.is_sales && order.salesperson_id == user.id && order.status.name == 'confirmed'
+          order.is_sales && order.salesperson_id == user.id && ['price_confirmed','confirmed'].include?(order.status.name)
         end
         
         can :update_info, Order do |order|
