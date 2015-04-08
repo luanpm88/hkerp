cc5145607fa2c6e1c1699789e29d4aa6a8cc235c
Luan Pham <luanpm88@gmail.com>
2015-04-07 22:20:38 -0700

    fix statistics

--- app/models/product.rb
+++ app/models/product.rb
@@ -169,7 +169,8 @@ class Product < ActiveRecord::Base
                         "<div class=\"text-center #{trashed_class}\">"+product.import_amount_formated(params[:year], params[:month]).to_s+'</div>',
                         "<div class=\"text-center #{trashed_class}\">"+product.export_count(params[:year], params[:month]).to_s+'</div>',
                         "<div class=\"text-center #{trashed_class}\">"+product.export_amount_formated(params[:year], params[:month]).to_s+'</div>',
-                        "<div class=\"text-center #{trashed_class}\">"+product.calculated_stock.to_s+'</div>',                        
+                        "<div class=\"text-center #{trashed_class}\">"+product.calculated_stock.to_s+'</div>',
+                        "<div class=\"text-center #{trashed_class}\">"+product.combination_count.to_s+'</div>',                        
                         ''
 
                       ]
@@ -542,6 +543,13 @@ class Product < ActiveRecord::Base
     link_helper.link_to '<i class="icon-time"></i> Product Activities'.html_safe, {controller: "products", action: "product_activity_history", id: self.id},target: "blank", :class => "btn btn-primary btn-mini"
     
     
+  end
+  
+  def combination_count
+    count = combinations.sum(:quantity).count
+    count -= combination_details.sum(:quantity).count
+    
+    return count
   end
   
 end
