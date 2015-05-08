5eaf77d38cc34257df0467a2667d10bef29af2af
Luan Pham <luanpm88@gmail.com>
2015-05-07 22:44:44 -0700

    filter by paid day

--- app/models/payment_record.rb
+++ app/models/payment_record.rb
@@ -12,6 +12,10 @@ class PaymentRecord < ActiveRecord::Base
   
   after_save :update_payment_status_name
   
+  def self.all_records
+    where(is_tip: false)
+  end
+  
   def update_payment_status_name
     order.update_payment_status_name
     order.update_tip_status_name
