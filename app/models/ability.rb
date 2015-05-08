4611a39871658e9fa5b81bb024ad039ed6d1f825
Luan Pham <luanpm88@gmail.com>
2015-05-08 01:07:14 -0700

    update uer add/edit form

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -267,6 +267,13 @@ class Ability
       can :do_update_info, Order do |order|
         ['new','items_confirmed','price_confirmed','confirmed','finished'].include?(order.status.name)
       end
+      
+      can :change, Order do |order|
+        order.is_purchase && order.purchaser_id == user.id && ['confirmed','finished'].include?(order.status.name)
+      end
+      can :do_change, Order do |order|
+        order.is_purchase && order.purchaser_id == user.id && ['confirmed','finished'].include?(order.status.name)
+      end
     end
 
     if user.has_role? "storage_manager"
