67f691feaf9560018266f012afa8cced1cb3d8d8
Luan Pham <luanpm88@gmail.com>
2015-10-05 13:34:23 +0700

    fixbug

--- app/models/product.rb
+++ app/models/product.rb
@@ -483,7 +483,9 @@ class Product < ActiveRecord::Base
     
     #count for sales delivery
     count -= order_details
+                      .joins(:delivery)
                       .joins(:order => :order_status)
+                      .where(deliveries: {status: 1})
                       .where(order_statuses: {name: ["finished"]})
                       .where(orders: {supplier_id: Contact.HK.id})
                       .where("orders.order_date < ?", datetime)
@@ -491,7 +493,9 @@ class Product < ActiveRecord::Base
     
     #count for purchase delivery
     count += order_details
+                      .joins(:delivery)
                       .joins(:order => :order_status)
+                      .where(deliveries: {status: 1})
                       .where(order_statuses: {name: ["finished"]})
                       .where(orders: {customer_id: Contact.HK.id})
                       .where("orders.order_date < ?", datetime)
