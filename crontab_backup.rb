3439a16db2c26c169dff97e73a320372e18f6690
Luan Pham <luanpm88@gmail.com>
2015-10-30 22:09:52 -0700

    backup

---
+++ crontab_backup.rb
@@ -0,0 +1,31 @@
+require 'active_record'
+require 'yaml'
+DIR = File.expand_path(File.dirname(__FILE__))
+rails_env = "development"
+# Change the following to reflect your database settings
+# config = YAML.load_file(DIR+'/config/database.yml')["production"]
+config = YAML.load_file(DIR+'/config/database.yml')[rails_env]
+
+puts DIR+'/config/database.yml'
+
+ActiveRecord::Base.establish_connection(
+  adapter: config["adapter"],
+  encoding: config["encoding"],
+  database: config["database"],
+  pool: 5,
+  username: config["username"],
+  password: config["password"]
+)
+
+#Item class
+require DIR+'/app/models/system.rb'
+
+System.backup({database: true, file: true, dir: DIR+"/", rails_env: rails_env})
+
+
+
+
+
+
+
+
