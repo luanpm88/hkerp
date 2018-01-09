8f65ce27dcf9688bd2dc7a4629c1513d5e8e2a32
Luan Pham <luanpm88@gmail.com>
2018-01-09 13:46:42 +0700

    update cache properties route

--- config/routes.rb
+++ config/routes.rb
@@ -253,7 +253,7 @@ Hkerp::Application.routes.draw do
       post :erp_set_imported
       post :erp_set_sold_out
       post :erp_set_cache_thcn_url
-      get :erp_set_cache_thcn_properties
+      post :erp_set_cache_thcn_properties
       get :erp_manufacturers_dataselect
 
       get :suspend
