19e1c3eb8fc85bbc903801600d250b14075124b4
Luan Pham <luanpm88@gmail.com>
2018-01-15 15:00:31 +0700

    contact search fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -285,7 +285,7 @@ class Contact < ActiveRecord::Base
     end
 
     q.split(" ").each do |k|
-      result = result.where("LOWER(contacts.cache_search) LIKE ?", "%#{k.strip.downcase}%")
+      result = result.where("LOWER(contacts.cache_search) LIKE ? OR LOWER(contacts.cache_search) LIKE ?", "%#{k.strip.downcase}%", "%#{k.strip.unaccent.downcase}%")
     end
 
     result = result.limit(50).map {|model| {:id => model.id, :text => model.short_name} }
